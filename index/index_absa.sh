5e5fb8795f1ebb16226ff8c862d8627d1f59301b
Mihalis Papakonstadinou <mihalispapak@gmail.com>
2016-10-19 13:34:27 +0300

    absa inde

--- index/index_absa.sh
+++ index/index_absa.sh
@@ -3,21 +3,21 @@
 content="$(head -n 1 $1)"
 
 type="$(echo ${content} | sed 's/{\"\(.*\)\": {/\1/')"
-content="$(cat $1 | sed 's/{\"$type\": {/{/' | sed 's/}}/}/')"
 
-cat $1 | sed "s/{\"$type\": {/{/" | sed 's/}}/}/'> tmp
+content="$(cat $1 | tr -d '\r\n' | sed 's/{\"$type\": {/{/' | sed 's/}}/}/')"
+
+cat $1 | sed "s/{\"$type\": {/{/" | sed 's/}}/}/' | tr -d '\r\n' | tr -d '\t' > tmp
 content="$(cat tmp)"
 
-id="$(cat $1 | grep \"id\": | sed 's/\"id\":\{1,\}//' | sed 's/\"//' | sed 's/\s\{1,\}//' | sed 's/,//' | sed 's/"//')"
+id="$(cat $1 | grep \"id\": | sed 's/\"id\":\{1,\}//' | sed 's/\"//' | sed 's/\s\{1,\}//' | sed 's/,//' | sed 's/\"//' | tr -d '\r\n' | tr -d '\t')"
+#echo '|'$id'|'
 
-echo '|'$id'|'
-
-exit
+#exit
 
 #TODO: think about given id, perhaps strip that from handle.net? seems like a good choice
-#echo "curl -XPUT 'http://localhost:9201/cimmyt/beta/12' -d '${content}'"
+#echo "curl -XPUT 'http://localhost:9201/sa_tweets/opinion/${id}' -d '${content}'"
 
-echo 'http://localhost:9201/cimmyt/resource/'$id
+#echo 'http://localhost:9201/cimmyt/resource/'$id
 #exit
 
 #curl -XPUT 'http://localhost:9201'$appuri -d "${content}"
